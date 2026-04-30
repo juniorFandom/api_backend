03e2261260055720175aa93d2656ea6a5f872cf2
juniorFandom <juniorfandom95@gmail.com>
2026-04-30 14:42:34 +0100

    feature:debut et implementation du CRUD sur le modul produit

--- app/produit/models.py
+++ app/produit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Produit(models.Model):
+    nom = models.CharField(max_length=100)
+    prix_vente = models.DecimalField(max_digits=6, decimal_places=2)
+    quantite_seuil = models.IntegerField()
+
+    def __str__(self):
+        return self.nom
+    
